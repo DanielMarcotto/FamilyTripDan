ffc821cc3050a0caf30ec576717f63b6cf8f5379
DanielMarcotto <daniel.marcotto@hotmail.it>
2026-04-18 08:51:52 +0200

    Update firebase.ts

--- server/src/database/firebase.ts
+++ server/src/database/firebase.ts
@@ -1,8 +1,7 @@
 import admin from "firebase-admin";
 
-console.log("ENV:", process.env.FIREBASE_SERVICE_ACCOUNT); 
-
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 if (!admin.apps.length) {
   admin.initializeApp({
