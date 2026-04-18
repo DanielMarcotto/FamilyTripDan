f13b3ea25ad429299bd829fbc633d148f634ede3
DanielMarcotto <daniel.marcotto@hotmail.it>
2026-04-18 08:56:05 +0200

    Update firebase.ts

--- server/src/database/firebase.ts
+++ server/src/database/firebase.ts
@@ -1,14 +1,22 @@
-import admin from "firebase-admin";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.db = void 0;
+const tslib_1 = require("tslib");
+const firebase_admin_1 = tslib_1.__importDefault(require("firebase-admin"));
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+try {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+    if (!firebase_admin_1.default.apps.length) {
+        firebase_admin_1.default.initializeApp({
+            credential: firebase_admin_1.default.credential.cert(serviceAccount),
+        });
+    }
+
+    const db = firebase_admin_1.default.firestore();
+    exports.db = db;
+} catch (err) {
+    console.error("FIREBASE INIT ERROR:", err);
+    throw err;
 }
-
-const db = admin.firestore();
-
-export { db };
